11fcb05643ed668eba486b0255cd30d3412a0e71
charlie_weng <charliexp.weng@gmail.com>
2016-09-21 15:25:37 +0800

    fix some bug
    Signed-off-by: charlie_weng <charliexp.weng@gmail.com>

---
+++ README.md
@@ -0,0 +1,33 @@
+# tcp-proxy
+
+[![build status](https://secure.travis-ci.org/jcrugzz/tcp-proxy.png)](http://travis-ci.org/jcrugzz/tcp-proxy)
+
+Exactly what you would expect, a simple tcp proxy written in node. Inspiration
+from [`node-http-proxy`][http-proxy].
+
+## Work in Progress
+
+## Example
+
+Simple tcp proxy
+
+```js
+
+var tcpProxy = require('tcp-proxy');
+
+var server = tcpProxy.createServer({
+  target: {
+    host: '127.0.0.1',
+    port: 9000
+  }
+});
+
+server.listen(8000);
+
+```
+
+## API
+
+
+
+[http-proxy]: https://github.com/nodejitsu/node-http-proxy
---
+++ example/simple-load-balancer.js
@@ -0,0 +1,30 @@
+var net      = require('net');
+var TcpProxy = require('../');
+
+///////////////////////////////////////////////////////////
+var targets = [
+    {
+        host: 'localhost',
+        port: 5000
+    } ,
+    {
+        host: 'localhost',
+        port: 5000
+    }
+];
+///////////////////////////////////////////////////////////
+var proxies = targets.map( function (target) {
+    return new TcpProxy( { target: target } );
+});
+
+var nextProxy = function () {
+    var proxy = proxies.shift();
+    proxies.push( proxy );
+    return proxy;
+};
+
+var server = net.createServer(function(socket) {
+    nextProxy().proxy(socket);
+});
+
+server.listen(8000);
